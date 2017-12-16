aa5da0e2c326d2b0da35d5cc7faf16037334671d
Anthony Phillip Cornejo <anthony.w.cornejo@gmail.com>
2017-12-16 13:05:20 -1000

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,117 @@
+public class LinkedList {
+	
+	private Node head;
+	
+	/*
+	 * The constructor creates an empty list.
+	 */
+	public LinkedList() {
+		head = null;
+	}
+	
+	/*
+	 * This method creates a new node and adds it to the head of the list.
+	 */
+	public void insertFirst(String data) {
+		Node newNode = new Node();
+		newNode.record = data;
+		newNode.next = head;
+		head = newNode;
+	}
+	
+	/*
+	 * This method deletes the first node in the list.
+	 */
+	public void deleteFirst() {
+		head = head.next;
+	}
+	
+	/*
+	 * This method should create a new node containing data and add it following the node 'after'.
+	 * If after is null, then it should do nothing.
+	 */
+	public void insertAfter(String data, Node after) {
+		Node pointer = head;
+		Node storage = new Node();
+		Node newNode = new Node();
+		newNode.record = data;
+		while(pointer != null && pointer.record != data) {
+			pointer = pointer.next;
+		}
+		if(pointer != null) {
+			pointer.next = newNode;
+		}
+	}
+	
+	/*
+	 * This method should find the node containing the string data and return a pointer to that node.
+	 * If not found, then it should return null.
+	 */
+	public Node findNode(String data) {
+		Node pointer = head;
+		while(pointer.record != data && pointer != null) {
+			pointer = pointer.next;
+		}
+		if(pointer.record == data) {
+			return pointer;
+		} else {
+			return null;
+		}
+	}
+	
+	/*
+	 * This method should delete the node trash.
+	 * If trash is null, then it does nothing.
+	 */
+	public void deleteNode(Node trash) {
+		Node pointer = head;
+		while(pointer.record != trash.record && pointer != null) {
+			pointer = pointer.next;
+		}
+		if(pointer.record == trash.record) {
+			pointer = null;
+		}
+		head = pointer;
+	}
+	
+	/*
+	 * This method should create a new node and insert it at the end of the list.
+	 */
+	public void insertLast(String data) {
+		Node storageNode = new Node();
+		storageNode.record = data;
+		Node pointer = head;
+		while(pointer.next != null) {
+			pointer = pointer.next;
+		} 
+		pointer.next = storageNode;
+		head = pointer;
+	}
+	
+	/*
+	 * This method deletes the last node in the list.
+	 * If the list is empty, then it does nothing.
+	 */
+	public void deleteLast() {
+		Node pointer = head;
+		while(pointer != null) {
+			if(pointer.next == null) {
+				pointer = null;
+				head = pointer;
+			}
+		}
+	}
+	
+	/*
+	 * This method transverses the list and prints out the record in each node.
+	 */
+	public void traverseAndPrint() {
+		Node pointer = head;
+		while (pointer != null) {
+			System.out.println(pointer.record);
+			pointer = pointer.next;
+		}
+		System.out.println();
+	}
+	
+}
